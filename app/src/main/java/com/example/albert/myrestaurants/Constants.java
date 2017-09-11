2302a41a55fbf7cd6af2adeffea302b4075b74d1
Okwonks <albertoketch@gmail.com>
2017-09-11 12:11:37 +0300

    Added Constants class to the project. This will help in managing the API requests to be made.

---
+++ app/src/main/java/com/example/albert/myrestaurants/Constants.java
@@ -0,0 +1,12 @@
+package com.example.albert.myrestaurants;
+
+/**
+ * Created by albert on 9/11/17.
+ */
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
