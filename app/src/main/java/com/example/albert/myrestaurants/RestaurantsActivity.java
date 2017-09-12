fa1e838b691ba48c6f91c66f705e848d6eb8daeb
Okwonks <albertoketch@gmail.com>
2017-09-12 11:44:12 +0300

    Adding fields to be able to make requests to the api.

--- app/src/main/java/com/example/albert/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/albert/myrestaurants/RestaurantsActivity.java
@@ -3,6 +3,7 @@ package com.example.albert.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 //import android.widget.ArrayAdapter;
@@ -10,13 +11,38 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    Log.v(TAG, jsonData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 
     private String[] restaurants = new String[] {"Que Pasa", "Artcaffe", "Kibandaski", "Amber Hotel",
@@ -44,5 +70,6 @@ public class RestaurantsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+        getRestaurants(location);
     }
 }
--- app/src/main/java/com/example/albert/myrestaurants/YelpService.java
+++ app/src/main/java/com/example/albert/myrestaurants/YelpService.java
@@ -5,8 +5,7 @@ import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
-import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
 
 /**
  * Created by albert on 9/11/17.
@@ -14,20 +13,17 @@ import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class YelpService {
     public static void findRestaurants(String location, Callback callback) {
-        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET);
-        consumer.setTokenWithSecret(Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new SigningInterceptor(consumer))
-                .build();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.YELP_BASE_URL).newBuilder();
         urlBuilder.addQueryParameter(Constants.YELP_LOCATION_QUERY_PARAMETER, location);
         String url = urlBuilder.build().toString();
 
         Request request = new Request.Builder()
+                .header("Authorization", Constants.YELP_TOKEN)
                 .url(url)
                 .build();
+
+        OkHttpClient client = new OkHttpClient();
 
         Call call = client.newCall(request);
         call.enqueue(callback);
