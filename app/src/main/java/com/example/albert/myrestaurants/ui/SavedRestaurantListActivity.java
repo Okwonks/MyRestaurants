1abbe0c708145beccfe176b0c38ab304cedf8807
Okwonks <albertoketch@gmail.com>
2017-09-20 14:58:53 +0300

    Adding SavedRestaurantListActivity which will display data from Firebase into the app.

--- app/src/main/java/com/example/albert/myrestaurants/ui/SavedRestaurantListActivity.java
+++ app/src/main/java/com/example/albert/myrestaurants/ui/SavedRestaurantListActivity.java
@@ -2,11 +2,51 @@ package com.example.albert.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.albert.myrestaurants.Constants;
+import com.example.albert.myrestaurants.R;
+import com.example.albert.myrestaurants.adapters.FirebaseRestaurantViewHolder;
+import com.example.albert.myrestaurants.models.Restaurant;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SavedRestaurantListActivity extends AppCompatActivity {
+    private DatabaseReference mRestaurantReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
+
+        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANT);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Restaurant, FirebaseRestaurantViewHolder>(Restaurant.class, R.layout.restaurant_list_item, FirebaseRestaurantViewHolder.class, mRestaurantReference) {
+            @Override
+            protected void populateViewHolder(FirebaseRestaurantViewHolder viewHolder, Restaurant model, int position) {
+                viewHolder.bindRestaurant(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
     }
 }
