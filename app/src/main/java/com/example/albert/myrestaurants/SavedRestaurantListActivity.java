ef0a4e4da78b0dfe1c69db7cc0633fda62105763
Okwonks <albertoketch@gmail.com>
2017-09-20 10:55:43 +0300

    Adding a new activity to handle data to be displayed from Firebase.

---
+++ app/src/main/java/com/example/albert/myrestaurants/SavedRestaurantListActivity.java
@@ -0,0 +1,13 @@
+package com.example.albert.myrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedRestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_restaurant_list);
+    }
+}
