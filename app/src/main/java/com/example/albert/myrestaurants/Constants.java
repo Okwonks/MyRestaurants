6c38c96fbffff35e2e54b588bcd1346b836d2c5c
Okwonks <albertoketch@gmail.com>
2017-09-20 07:14:24 +0300

    Adding google-services to the app. This will deal with the connection to Firebase.

--- app/src/main/java/com/example/albert/myrestaurants/Constants.java
+++ app/src/main/java/com/example/albert/myrestaurants/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
     public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurant";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCE_LOCATION_KEY = "location";
+    public static final String FIREBASE_SEARCHED_CHILD_LOCATION = "searchedLocation";
 }
